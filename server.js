dbaf46499331e87b8e7556c6c3303c3681c5d617
akachi <Prokason@gmail.com>
2022-06-23 10:57:48 +0100

    hjhj

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
@@ -16,4 +17,11 @@ app.listen(port, () => {
  
   });
   console.log(`Server is running on port: ${port}`);
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
